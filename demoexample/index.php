b693101b3760aa5e39e18c90c17f3b1174127a5f
dkanojia <dkanojia93@gmail.com>
2017-08-22 18:23:53 +0530

    first commit

--- demoexample/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once('main_index.php');
-// include_once('popuppage.php');
-// include_once('globaljs.php');
-include_once('index_js.php');
-
-?>
--- drupal-7.56 - Copy - Copy/availUser.php
+++
@@ -1,7 +0,0 @@
-Users : 
-
-First User : drupalUser
-Pwd : drupal123
-
-Second User : siteAdmin
-Pwd : 123456
